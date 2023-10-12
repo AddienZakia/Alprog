bb3bded7956372f10a1c2d94e5df6243d9ca31f4
AddienZakia <addienzakia1903@gmail.com>
2023-10-12 15:15:04 +0700

    Program

---
+++ Program/Matriks.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Matriks {
+    public static void main(String[] args) {
+        
+        int m, n;
+        Scanner data = new Scanner(System.in);
+
+        System.out.print("Masukkan ukuran baris Matriks : ");
+        m = data.nextInt();
+
+        System.out.print("Masukkan ukuran kolom Matriks : ");
+        n = data.nextInt();
+
+        data.close();
+
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                System.out.printf("%4d", i*j);
+            }
+            System.out.println();
+        }
+    }
+}
--- file.java
+++ file.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * file
  */
@@ -13,11 +15,96 @@ public class file {
 
         // System.out.println("Sum is " + sum);
 
-        int sum = 0, i = 1;
+        // int sum = 0, i = 1;
+        // do {
+        //     sum += i;
+        //     i++;
+        // } while (i < 10);
+        // System.out.println(sum);
+        
+        // int i = 0, sum = 0;
+        // while (i < 5) {
+        //     for (int j = 0; j < 6; j++) {
+        //         System.out.print("Nilai i : " + i + ", Nilai j : " + j + ", Nilai sum : " + sum);
+        //         sum += i+j;
+        //         System.out.println(", Hasil : " + sum);
+        //     }
+        //     i++;
+        // }
+
+        // System.out.println("Total : " + sum);
+
+
+        // int m, n;
+        // Scanner data = new Scanner(System.in);
+
+        // System.out.print("Masukkan ukuran baris Matriks : ");
+        // m = data.nextInt();
+
+        // System.out.print("Masukkan ukuran kolom Matriks : ");
+        // n = data.nextInt();
+
+        // data.close();
+
+        // for (int i = 0; i <= m; i++) {
+        //     for (int j = 0; j <= n; j++) {
+        //         System.out.printf("%4d", i*j);
+        //     }
+        //     System.out.println();
+        // }
+
+
+        int kolomA = 0, barisA = 0, kolomB = 0, barisB = 0;
+        Scanner data = new Scanner(System.in);
+
         do {
-            sum += i;
-            i++;
-        } while (i < 10);
-        System.out.println(sum);
+            System.out.print("Masukkan jumlah kolom matriks A : ");
+            kolomA = data.nextInt();
+
+            System.out.print("Masukkan jumlah baris matriks A : ");
+            barisA = data.nextInt();
+
+            System.out.print("Masukkan jumlah kolom matriks B : ");
+            kolomB = data.nextInt();
+
+            System.out.print("Masukkan jumlah baris matriks B : ");
+            barisB = data.nextInt();
+        } while (kolomA != kolomB || barisA != barisB);
+
+        data.close();
+
+        int[][] matrixA = new int[kolomA][barisA];
+        int[][] matrixB = new int[kolomB][barisB];
+
+        int sum = 0;
+        for (int i = 0; i < kolomA; i++) {
+            for (int j = 0; j < barisA; j++) {
+                matrixA[i][j] = sum;
+                sum++;
+                System.out.printf("%4d", sum);
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        int sumB = 0;
+        for (int i = 0; i < kolomB; i++) {
+            for (int j = 0; j < barisB; j++) {
+                matrixB[i][j] = sumB;
+                sumB++;
+            System.out.printf("%4d", sumB);
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        
+        for (int i = 0; i < kolomB; i++) {
+            for (int j = 0; j < barisB; j++) {
+                System.out.printf("%4d", matrixA[i][j] + matrixB[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
