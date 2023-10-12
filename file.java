ffde115aa402f25573228013222282465af91d38
AddienZakia <addienzakia1903@gmail.com>
2023-10-12 14:05:18 +0700

    Program

--- file.java
+++ file.java
@@ -4,6 +4,20 @@
 public class file {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        // int sum = 0, i = 1;
+
+        // while (i < 10) {
+        //     sum += i;
+        //     i++;
+        // }
+
+        // System.out.println("Sum is " + sum);
+
+        int sum = 0, i = 1;
+        do {
+            sum += i;
+            i++;
+        } while (i < 10);
+        System.out.println(sum);
     }
 }
