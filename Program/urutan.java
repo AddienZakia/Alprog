1b9b28e18b4733f1a2608cdd2558919ca8dcfc57
AddienZakia <addienzakia1903@gmail.com>
2023-11-17 14:00:32 +0700

    Program

--- Program/urutan.java
+++ Program/urutan.java
@@ -4,17 +4,17 @@ public class urutan {
     public static void main(String[] args) {
 
         // geser ke kiri
-        // int[] a = { 1, 2, 3, 4, 5 };
+        int[] a = { 1, 2, 3, 4, 5 };
 
-        // int temp = a[0];
+        int temp = a[0];
 
-        // for (int i = 1; i < a.length; i++) {
-        // a[i - 1] = a[i];
-        // }
+        for (int i = 1; i < a.length; i++) {
+            a[i - 1] = a[i];
+        }
 
-        // a[a.length - 1] = temp;
+        a[a.length - 1] = temp;
 
-        // System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(a));
 
         // geser ke kanan
         int[] ax = { 1, 2, 3, 4, 5 };
